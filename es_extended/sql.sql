5903397d24ffa50f5d0528e3cefdc799df8dc10c
DevlopementShir0gang <70062169+DevlopementShir0gang@users.noreply.github.com>
2020-12-12 04:09:26 +0100

    Add files via upload

---
+++ es_extended/sql.sql
@@ -0,0 +1,14 @@
+ALTER TABLE `users`
+
+	ADD COLUMN `job2` varchar(255) NULL DEFAULT 'unemployed2' AFTER `job_grade`,
+
+	ADD COLUMN `job2_grade` INT NULL DEFAULT 0 AFTER `job2`
+
+;
+
+
+INSERT INTO `jobs` (`name`, `label`) VALUES
+('unemployed2', 'Sans emplois');
+
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('unemployed2', 0, 'rsa', 'Secondaire', 50, '{}', '{}');
